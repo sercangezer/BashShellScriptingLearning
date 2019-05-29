5103a057adfab055841c8e7b43e72a4540729e3d
Sercan GEZER <sercangezer.tr@gmail.com>
2019-05-29 09:29:35 +0300

    Examples at second days.

--- case-scripts.sh
+++ case-scripts.sh
@@ -2,4 +2,24 @@
 
 # Author: Sercan GEZER
 
+clear #Clear terminal screen
+
+echo
+echo "Please choose one of the options below"
+echo
+echo "a = Display Date and Time"
+echo "b = List file and directory"
+echo "c = List users logged in"
+echo "d = Check system uptime"
+echo
+
+read choice
+
+case $choice in
+	a) date;;
+	b) ls -l;;
+	c) who;;
+	d) uptime;;
+	*) echo "Invalid choice. Bye..."
+esac
 
---
+++ for-example01.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+# Author: Sercan GEZER
+# Date created: 28.05.2019
+
+for i in 1 2 3 4 5
+do
+	echo "Welcome $i times"
+done
